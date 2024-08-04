1bafeb2c12d69a24d6c7267f4f539ec4c163f23a
Abdelrhman <abdu1234680@gmail.com>
2024-08-04 15:38:27 +0300

    Add Bootstrap and React Router Dom for Enhanced UI and Navigation
    This commit introduces Bootstrap version 5.3.3 and React Router Dom version 6.26.0 to the project, enhancing the UI components and enabling better navigation capabilities. Bootstrap's CSS and JavaScript files are now included, providing a robust set of styles and interactive elements. React Router Dom is integrated to manage routes within the application, allowing for a more dynamic and responsive user experience.
    
    Changes include:
    - Updated `package.json` to include Bootstrap and React Router Dom as dependencies.
    - Modified `package-lock.json` to reflect the new dependencies and their versions.
    - Integrated Bootstrap's CSS and JS in `index.js` for global application styling and functionality.
    - Added React Router Dom's `BrowserRouter`, `Routes`, and `Route` components in `App.js` to define and manage application routes.
    - Created a new `About` component for demonstration of routing functionality.
    - Updated the `navbar.js` component to use React Router Dom's `Link` for navigation, replacing traditional anchor tags.
    - Removed the `logo.svg` file as it is no longer used in the application.
    
    These changes aim to improve the project's UI/UX by leveraging Bootstrap's extensive toolkit and enabling seamless navigation with React Router Dom.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import Navebar from "./components/navbar";
 import Slider from "./components/slider";
 import ProductsList from "./components/productsList";
+import About from "./components/about";
+import { Routes , Route } from "react-router-dom"; 
 function App() {
   return (
     <div className="App">
     <Navebar />
-    <Slider />
-
-    <ProductsList/>
+    <Routes>
+      <Route path="/" element={
+        <>
+        <Slider />
+        <ProductsList/>
+        </>
+      } />
+        <Route path="about" element={<About/>} />
+    </Routes>
     </div> 
   );
 }
