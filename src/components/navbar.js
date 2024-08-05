86082bca039bb9ca5a1b6688dbba2eada1045593
Abdelrhman <abdu1234680@gmail.com>
2024-08-05 18:36:53 +0300

    Update navbar brand text to "E-commerce"
    Changed the navbar brand text from "Zilong" to "E-commerce" for better alignment with the application's purpose. This update enhances user understanding of the platform's focus and improves the overall branding consistency.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,7 +4,7 @@ function Navebar(){
        <>
        <nav className="navbar navbar-expand-lg bg-light">
   <div className="container">
-    <Link className="navbar-brand" to ="/">Zilong</Link>
+    <Link className="navbar-brand" to ="/">E-commerce</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
